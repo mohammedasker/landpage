9f7e9bfcb8a953756983bc0158d8ccf2f2cdfe88
Mohammed Asker <zetoanime@gmail.com>
2020-01-18 17:20:52 +0300

    Updates

---
+++ precache-manifest.e85274ebd800151df6bb8e4bdedc2eee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3dce4bc9f078eb48effaf9368a3ce5",
+    "url": "/landpage/index.html"
+  },
+  {
+    "revision": "9789f2c10f12f1b03345",
+    "url": "/landpage/static/css/main.0f780ebd.chunk.css"
+  },
+  {
+    "revision": "ffbb4ec099a9205b1e52",
+    "url": "/landpage/static/js/2.1389ac8d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/landpage/static/js/2.1389ac8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9789f2c10f12f1b03345",
+    "url": "/landpage/static/js/main.cd1b21db.chunk.js"
+  },
+  {
+    "revision": "63202d3f426ea7e06927",
+    "url": "/landpage/static/js/runtime-main.c4f6adff.js"
+  }
+]);
