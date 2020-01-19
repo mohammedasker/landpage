28c7cfeaf0a7b38842900442869e56d7644a4ecb
Mohammed Asker <zetoanime@gmail.com>
2020-01-19 12:37:22 +0300

    made video responsive - need fix later

--- src/components/feature-video/video.jsx
+++ src/components/feature-video/video.jsx
@@ -4,6 +4,8 @@ import './video.style.css'
 export const Video = () => {
     return <div id="promotion" className='video-section'>
         <h1>And the biggest reason to own this chair is...</h1>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/B1VvJhquVWU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/B1VvJhquVWU" frameborder="0"></iframe>
+        </div>
     </div>
 }
